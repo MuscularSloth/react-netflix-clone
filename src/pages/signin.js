3609ff620c3bb687bd39049509cf717b7070d86a
Konstantin Makar <kansiweb@gmail.com>
2022-01-30 12:40:30 +0200

    Add Sign In Page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
-export default function Signin(){
-    return (
-        <>
-            Signin
-        </>
-    )
+export default function Signin() {
+	const navigate = useNavigate();
+	const { firebase } = useContext(FirebaseContext);
+	const [emailAddress, setEmailAddress] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+
+	const isInvalid = password === "" || emailAddress === "";
+
+	const handleSignin = (e) => {
+		e.preventDefault();
+
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(emailAddress, password)
+			.then(() => {
+				navigate(ROUTES.ROUTE_BROWSE);
+			})
+			.catch((error) => {
+				setEmailAddress("");
+				setPassword("");
+				setError(error.message);
+			});
+	};
+
+	return (
+		<>
+			<HeaderContainer>
+				<Form>
+					<Form.Title>Sign In</Form.Title>
+					{error && <Form.Error>{error}</Form.Error>}
+
+					<Form.Base onSubmit={handleSignin} method="POST">
+						<Form.Input
+							placeholder="Email address"
+							value={emailAddress}
+							onChange={({ target }) => setEmailAddress(target.value)}
+						/>
+						<Form.Input
+							placeholder="Password"
+							type="password"
+							autoComplete="off"
+							value={password}
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+						<Form.Submit disabled={isInvalid} type="submit">
+							Sign In
+						</Form.Submit>
+					</Form.Base>
+					<Form.Text>
+						New to Netflix?{" "}
+						<Form.Link to={ROUTES.ROUTE_SIGN_UP}>Sign up now.</Form.Link>
+					</Form.Text>
+					<Form.TextSmall>
+						This page protected by Google reCAPTCHA to ensure you'r not a bot.{" "}
+						<a target="_blank" rel="noreferrer" href="https://google.com">
+							Learn more.
+						</a>
+					</Form.TextSmall>
+				</Form>
+			</HeaderContainer>
+			<FooterContainer />
+		</>
+	);
 }
