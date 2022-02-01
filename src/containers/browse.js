503a6e7cb5efcd40f812c969ed16b848835ecc82
Konstantin Makar <kansiweb@gmail.com>
2022-02-01 17:39:26 +0200

    Add player

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
-import { Card, Header, Loading } from "../components";
+import { Card, Header, Loading, Player } from "../components";
 import { FooterContainer } from "./footer";
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
@@ -109,10 +109,10 @@ export default function BrowseContainer({ slides }) {
 									))}
 								</Card.Entities>
 								<Card.Feature category={category}>
-									{/* <Player>
+									<Player>
 										<Player.Button />
 										<Player.Video src="/videos/bunny.mp4" />
-									</Player> */}
+									</Player>
 								</Card.Feature>
 							</Card>
 						))}
